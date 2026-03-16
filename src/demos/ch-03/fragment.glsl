c09d976418572da8162a6587d9936d74c2cbbc6e
iwasaki <poettype73@gmail.com>
2026-03-17 01:14:43 +0900

    Add ch-03

---
+++ src/demos/ch-03/fragment.glsl
@@ -0,0 +1,39 @@
+#version 300 es
+precision mediump float;
+
+in vec2 vUv;
+out vec4 fragColor;
+
+uniform vec2 uResolution;
+
+float PI = 3.1415926;
+
+float aastep(float threshold, float dist) {
+  float afwidth = 0.7 * length(vec2(dFdx(dist), dFdy(dist)));
+  return smoothstep(threshold - afwidth, threshold + afwidth, dist);
+}
+
+void main() {
+  vec2 uv = vUv;
+  vec2 pos = uv * uResolution / min(uResolution.x, uResolution.y);
+
+  float freq = 10.0;
+
+  float angle = PI / 4.0;
+  mat2 rot = mat2(
+    cos(angle), -sin(angle),
+    sin(angle), cos(angle)
+  );
+
+  vec2 st = rot * pos;
+  vec2 near = 2.0 * fract(freq * st) - 1.0;
+
+  float dist = length(near);
+  float radius = 0.5;
+
+  vec3 white = vec3(1.0);
+  vec3 black = vec3(0.0);
+  vec3 color = mix(black, white, aastep(radius, dist));
+
+  fragColor = vec4(color, 1.0);
+}
