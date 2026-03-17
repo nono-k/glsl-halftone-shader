29819377a87b34071eaa18623c2d47e8f5e98767
iwasaki <poettype73@gmail.com>
2026-03-18 00:42:12 +0900

    Add ch-07

---
+++ src/demos/ch-07/fragment.glsl
@@ -0,0 +1,49 @@
+#version 300 es
+precision mediump float;
+
+in vec2 vUv;
+out vec4 fragColor;
+
+uniform vec2 uResolution;
+uniform sampler2D uTexture;
+
+float PI = 3.1415926;
+
+#include ../noise/noise.glsl
+
+float aastep(float threshold, float dist) {
+  float afwidth = 0.7 * length(vec2(dFdx(dist), dFdy(dist)));
+  return smoothstep(threshold - afwidth, threshold + afwidth, dist);
+}
+
+void main() {
+  vec2 uv = vUv;
+  vec2 pos = uv * uResolution / min(uResolution.x, uResolution.y);
+
+  float freq = 40.0;
+
+  float angle = PI / 4.0;
+  mat2 rot = mat2(
+    cos(angle), -sin(angle),
+    sin(angle), cos(angle)
+  );
+
+  vec2 st = rot * pos;
+  vec2 near = 2.0 * fract(freq * st) - 1.0;
+
+  float dist = length(near);
+  vec3 textureColor = texture(uTexture, uv).rgb;
+  float gray = dot(textureColor, vec3(0.299, 0.587, 0.114));
+
+  float radius = sqrt(1.0 - gray);
+
+  float n = 0.1 * snoise(pos * 200.0);
+  n += 0.05 * snoise(pos * 400.0);
+  n += 0.025 * snoise(pos * 800.0);
+
+  vec3 white = vec3(n * 0.5 + 0.98);
+  vec3 black = vec3(n + 0.1);
+  vec3 color = mix(black, white, aastep(radius, dist + n));
+
+  fragColor = vec4(color, 1.0);
+}
---
+++ src/demos/ch-07/index.ts
@@ -0,0 +1,72 @@
+import { Renderer, Camera, Transform, Texture, Program, Mesh, Geometry, Vec3, Orbit } from "ogl";
+
+import fragment from './fragment.glsl';
+import vertex from './vertex.glsl?raw';
+
+import image from '../../images/img-01.jpg';
+
+const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+
+const renderer = new Renderer({ canvas });
+const gl = renderer.gl;
+
+const camera = new Camera(gl, { fov: 45 });
+camera.position.set(0, 0, 5);
+
+const controls = new Orbit(camera, {
+  target: new Vec3(0, 0, 0),
+  element: canvas
+})
+
+const scene = new Transform();
+const texture = new Texture(gl);
+
+const img = new Image();
+img.src = image;
+img.onload = () => texture.image = img;
+
+
+const geometry = new Geometry(gl, {
+  position: { size: 3, data: new Float32Array([-1, -1, 0, 1, -1, 0, 1, 1, 0, -1, 1, 0]) },
+  uv: { size: 2, data: new Float32Array([0, 0, 1, 0, 1, 1, 0, 1]) },
+  index: { data: new Uint16Array([0, 1, 2, 0, 2, 3]) },
+});
+
+const program = new Program(gl, {
+  vertex,
+  fragment,
+  uniforms: {
+    uResolution: { value: [gl.canvas.width, gl.canvas.height] },
+    uTexture: { value: texture },
+  }
+});
+
+const mesh = new Mesh(gl, { geometry, program });
+mesh.setParent(scene);
+
+function resize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.perspective({ aspect: gl.canvas.width / gl.canvas.height });
+  const distance = camera.position.z
+  const fov = camera.fov * Math.PI / 180
+
+  const height = 2 * Math.tan(fov / 2) * distance
+  const width = height * camera.aspect
+
+  mesh.scale.set(width / 2, height / 2, 1)
+}
+
+window.addEventListener("resize", resize, false);
+resize();
+
+requestAnimationFrame(update);
+
+function update() {
+  requestAnimationFrame(update);
+  controls.update();
+  program.uniforms.uResolution.value = [
+    gl.canvas.width,
+    gl.canvas.height
+  ];
+  renderer.render({ scene, camera });
+}
