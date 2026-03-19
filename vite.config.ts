1d3938f34d96a9424f2e2b5a2af55069c4775db9
iwasaki <poettype73@gmail.com>
2026-03-19 11:37:49 +0900

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
-  plugins: [glsl()],
+  plugins: [
+    glsl({
+      include: ["**/*.glsl", "**/*.frag", "**/*.vert"]
+    })
+  ]
 });
