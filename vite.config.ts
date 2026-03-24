671462f76c3980c66fcf63afd378ef31ae2ae376
iwasaki <poettype73@gmail.com>
2026-03-24 12:51:02 +0900

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/glsl-halftone-shader/",
+  build: {
+    rolldownOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        demo: resolve(__dirname, "demo.html")
+      }
+    }
+  },
   plugins: [
     glsl({
       include: ["**/*.glsl", "**/*.frag", "**/*.vert"]
